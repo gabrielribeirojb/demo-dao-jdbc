58fb5239f7c2ec903bd2acae6cfce292e4f4f769
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2020-12-08 20:54:34 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -49,7 +49,7 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
 		
-		System.out.println("\n================== TEST 5: seller update ==================");
+		System.out.println("\n================== TEST 6: seller delete ==================");
 		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
